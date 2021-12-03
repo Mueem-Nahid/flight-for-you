b001d04c71e4630753562188fd6d20f95acb0830
Mueem-Nahid <mueem51@gmail.com>
2021-12-03 18:54:06 +0600

    nothing

--- src/components/MainSection/MainSection.vue
+++ src/components/MainSection/MainSection.vue
@@ -14,25 +14,30 @@
                 <div class="card-body">
                   <h5 class="card-title">Leaving from</h5>
                   <h4 class="card-text">DAC</h4>
-                  <h5 class="card-text">Dhaka</h5>
+                  <h5 class="card-text">{{ leavingFrom }}</h5>
                 </div>
               </div>
-              <div class="card btn" data-bs-toggle="modal"
-                data-bs-target="#exampleModal">
+              <div
+                class="card btn"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
                 <div class="card-body">
                   <h5 class="card-title">Going to</h5>
                   <h4 class="card-text">DAC</h4>
-                  <h5 class="card-text">Dhaka</h5>
+                  <h5 class="card-text">{{ goingTo }}</h5>
                   <p class="card-text"></p>
                 </div>
               </div>
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Departing on</h5>
+                  <input type="date" v-model="departingOn" />
                   <h5 class="card-title">Returning on</h5>
+                  <input type="date" v-model="returningOn" />
                 </div>
               </div>
-              <div class="card">
+              <div class="card btn btn-light" @click="search">
                 <div class="card-body">
                   <h5 class="card-title">Search Flight</h5>
                   <p class="card-text"></p>
@@ -50,6 +55,41 @@
         </div>
       </div>
     </div>
+
+    <!-- flights -->
+    <!-- <div v-for="item in all" :key="item">
+      <h3>
+        destination code:
+        {{ all.response.flights[item].flight[0].destinationName.code }}
+      </h3>
+    </div> -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Destination code</th>
+          <th scope="col">origin Iata Code</th>
+          <th scope="col">destinationTime</th>
+          <th scope="col">originTime</th>
+          <th scope="col">duration</th>
+          <th scope="col">price</th>
+          <th scope="col">airlineCode</th>
+          <th scope="col">flightNumber</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ destinationIataCode }}</td>
+          <td>{{ originIataCode }}</td>
+          <td>{{ destinationTime }}</td>
+          <td>{{ originTime }}</td>
+          <td>{{ duration }}</td>
+          <td>{{ price }}</td>
+          <td>{{ airlineCode }}</td>
+          <td>{{ flightNumber }}</td>
+        </tr>
+      </tbody>
+    </table>
+
     <!-- Modal -->
     <div
       class="modal fade"
@@ -79,6 +119,7 @@
                 class="form-control"
                 id="leavingFrom"
                 aria-describedby="emailHelp"
+                v-model="leavingFrom"
               />
             </div>
             <div class="">
@@ -88,8 +129,17 @@
                 class="form-control"
                 id="goinTo"
                 aria-describedby="emailHelp"
+                v-model="goingTo"
               />
             </div>
+
+            <!-- <div v-if="suggestions">
+              <ul>
+                <li v-for="suggestion in suggestions" :key="suggestion">
+                  {{ suggestion }}
+                </li>
+              </ul>
+            </div> -->
           </div>
           <div class="modal-footer">
             <button
@@ -106,3 +156,88 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      leavingFrom: "",
+      goingTo: "",
+      departingOn: "",
+      returningOn: "",
+      destinationIataCode: "",
+      originIataCode: "",
+      destinationTime: "",
+      originTime: "",
+      duration: "",
+      price: "",
+      airlineCode: "",
+      flightNumber: "",
+    };
+  },
+
+  methods: {
+    search() {
+      //var maindata;
+      // var destinationIataCode;
+      // var originIataCode;
+      // var destinationTime;
+      // var originTime;
+      // var duration;
+      // var price;
+      // var airlineCode;
+      // var flightNumber;
+
+      console.log(
+        this.leavingFrom,
+        this.goingTo,
+        this.departingOn,
+        this.returningOn
+      );
+      var url =
+        "https://api.sharetrip.net/api/v1/flight/search?tripType=Return&adult=1&child=0&infant=0&class=Economy&origin=" +
+        this.leavingFrom +
+        "&destination=" +
+        this.goingTo +
+        "&depart=" +
+        this.departingOn +
+        "&depart=" +
+        this.returningOn;
+
+      console.log(url);
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.destinationIataCode =
+            data.response.flights[0].flight[0].destinationName.code;
+          this.originIataCode =
+            data.response.flights[0].flight[0].originName.code;
+          (this.destinationTime =
+            data.response.flights[0].flight[0].departureDateTime.time),
+            (this.originTime =
+              data.response.flights[0].flight[0].arrivalDateTime.time);
+          this.duration = data.response.flights[0].flight[0].duration;
+          this.price = data.response.flights[0].price;
+          this.airlineCode =
+            data.response.flights[0].segments[0].segment[1].airlines.code;
+          this.flightNumber =
+            data.response.flights[0].segments[0].segment[1].flightNumber;
+        });
+    },
+    /*autoSug() {
+      let url =
+        "https://api.sharetrip.net/api/v1/flight/search/airport?name=" +
+        this.leavingFrom;
+      console.log(url);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.response[0]);
+          this.suggestions = data.response[0].name;
+        });
+    },*/
+  },
+};
+</script>
